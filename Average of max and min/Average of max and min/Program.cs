33b254c0e7fd50f964471e951c427ea7995bcd0f
i-revan <rvaniskndrov03@gmail.com>
2023-02-26 01:25:31 -0800

    created average of max and min

---
+++ Average of max and min/Average of max and min/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Average_of_max_and_min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 30;
+            int c = 10;
+            int min = a;
+            int max = a;
+            if(b < min)
+            {
+                min = b;
+            }
+            if(c < min)
+            {
+                min = c;
+            }
+            if(b > max)
+            {
+                max = b;
+            }
+            if(c > max)
+            {
+                max = c;
+            }
+            Console.WriteLine((max + min) / 2);
+        }
+    }
+}
